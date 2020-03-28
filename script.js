4c1b5239f6c8da6f6d7d1ec91cdeee2600309eec
prostiak2019 <yarovenkodmv@gmail.com>
2020-03-28 22:54:07 +0300

    fix: DOM

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onload = function() {
   console.log('All right !!!');
 
   // NavLinks
-
-  selectNavLink();
+   
+   changeSelectionByScrolling();
+   selectNavLink();  
 
   // Slider
 
@@ -16,9 +17,38 @@ window.onload = function() {
   switchPortfolioTag();
   selectPortfolioItem();
 
+  // Get a Quote
+
+  submitForm();
+
+
 }
 
 // NavLinks
+
+
+const changeSelectionByScrolling = () => {
+  document.addEventListener('scroll', onScroll);
+
+  function onScroll() {
+    const currentPos = window.scrollY + 95;
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.main-menu a');
+
+    sections.forEach((el) => {
+      if ((el.offsetTop <= currentPos) && ((el.offsetTop + el.offsetHeight) > currentPos)) {
+        navLinks.forEach((a) => {
+          a.classList.remove('main-menu__link_selected');
+          a.classList.add('main-menu__link_unselected'); 
+          if (el.getAttribute('class').includes(a.getAttribute('href').substring(1))) {
+            a.classList.remove('main-menu__link_unselected');
+            a.classList.add('main-menu__link_selected');
+          }
+        })
+      }
+    })
+  }
+};
 
 const selectNavLink = () => {
   document.querySelector('.main-menu').addEventListener('click', (e) => {
@@ -28,7 +58,7 @@ const selectNavLink = () => {
       selectClickedNavLink(clickedNavLink);
     }
   })
-}
+};
 
 const removeSelectedNavLink = () => {
   let navLinks = document.querySelectorAll('.main-menu .main-menu__link');  
@@ -36,12 +66,12 @@ const removeSelectedNavLink = () => {
     link.classList.remove('main-menu__link_selected');
     link.classList.add('main-menu__link_unselected');    
   });
-}
+};
 
 const selectClickedNavLink = (clickedNavLink) => {  
   clickedNavLink.classList.remove('main-menu__link_unselected');
   clickedNavLink.classList.add('main-menu__link_selected');
-}
+};
 
  // Slider
  
@@ -52,7 +82,7 @@ const switchPhoneScreens = () => {
   document.querySelector('.phone_horizontal').addEventListener('click', () => {
     switchScreen('horizontal');     
   })
-}
+};
 
 const switchScreen = (position) => { 
   const verticalFrame = document.querySelector('.vertical-frame');
@@ -71,9 +101,7 @@ const switchScreen = (position) => {
       horizontalFrame.classList.add('display-none');
     }
   }
-}
-
-// Slider
+};
 
 const switchSlider = () => {
 
@@ -138,7 +166,7 @@ const switchSlider = () => {
       changeStripColor (currentItem)
     }
   });  
-}
+};
 
 //  Portfolio
 
@@ -151,26 +179,21 @@ const switchPortfolioTag = () => {
       changePortfolioItem(clickedTag);
     }
   })
-}
+};
 
 const removeSelectedTag = () => {
   let tags = document.querySelectorAll('.portfolio__buttons .button');  
   tags.forEach( item => {    
     item.classList.remove('button-selected');
   });
-}
+};
 
 const selectClickedTag = (clickedTag) => { 
   clickedTag.classList.add('button-selected');
-}
+};
 
 const changePortfolioItem = (clickedTag) => {
-  
-
-}
-
-
-//  Portfolio
+};
 
 const selectPortfolioItem = () => {
   document.querySelector('.four-columns').addEventListener('click', (e) => {
@@ -180,21 +203,68 @@ const selectPortfolioItem = () => {
       selectClickedPortfolioItem(clickedPortfolioItem);
     }
   })
-}
+};
 
 const removeSelectedPortfolioItem = () => {
   let PortfolioItems = document.querySelectorAll('.four-columns .portfolio__column-item');  
   PortfolioItems.forEach( item => {    
     item.classList.remove('portfolio__column-item_selected');
   });
-}
+};
 
 const selectClickedPortfolioItem = (clickedPortfolioItem) => { 
   clickedPortfolioItem.classList.add('portfolio__column-item_selected');
-}
+};
 
 
+// Get a Quote
 
+const submitForm = () => {
+  const submitButton = document.querySelector('.feedback-form__submit');
+  const okButton = document.querySelector('.modal-popup__button');
+  
+  const modal = document.querySelector('.modal');
+
+  const userName = document.querySelector('.feedback-form__name');
+  const userEmail = document.querySelector('.feedback-form__email');
+  const userSubject = document.querySelector('.feedback-form__subject');
+  const userMessage = document.querySelector('.feedback-form__textarea');  
+
+  submitButton.addEventListener('click', (event) => {
+    
+    if ((userName.value !== '') && (userEmail.value !== '')) {
+      event.preventDefault();
+    document.body.style.overflowY = 'hidden';
+      document.querySelector('.modal-popup__result').innerText = 'The letter was sent';      
+
+      const subjectText = userSubject.value.toString();
+      const messageText = userMessage.value.toString();
+
+      if (subjectText !== '') {
+        document.querySelector('.modal-popup__subject-text').innerText ='Subject: ' + subjectText;
+      } else {
+        document.querySelector('.modal-popup__subject-text').innerText ='No subject';
+      }
+
+      if (messageText !== '') {
+        document.querySelector('.modal-popup__message-text').innerText ='Description: ' + messageText;
+      } else {
+        document.querySelector('.modal-popup__message-text').innerText ='No description';
+      }
+
+      modal.classList.remove('modal_hidden');
+      modal.classList.add('modal_visible');
+    }
+  });
+
+  okButton.addEventListener('click', (event) => {
+    document.body.style.overflowY = '';
+    document.querySelector('.feedback-form').reset();
+    document.querySelector('.modal-popup__result').innerText = '';
+    modal.classList.remove('modal_visible');
+    modal.classList.add('modal_hidden');
+  });
+};
 
 // console.log('All right 1 !!!');
 // console.log('All right 2 !!!');
